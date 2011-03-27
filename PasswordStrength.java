7a038af4b9d0f0ba7a25986f418ad16a234a1939
John Stitt <jmstitt@gmail.com>
2011-03-27 14:32:52 -0500

    Added functionality: allow password to be passed in as argument.

--- PasswordStrength.java
+++ PasswordStrength.java
@@ -7,26 +7,35 @@
 import java.util.*;
 
 public class PasswordStrength {
+        
+        final static double POSSIBLECHARS = 62; //Upper and Lower Alphebet (52), Arabic Numberals (10)
+        
 	public static void main( String[] args ){
-		final double POSSIBLECHARS = 62; //Upper and Lower Alphabet (52), Arabic Numerals (10)
 		double entropy = 0;
 		int pwlength = 0;
+		String password;
 
-		Scanner scan = new Scanner( System.in );
-		System.out.print( "Enter a password (numbers and characters, not case sensitive): ");
+                if( args.length > 0 ){
+                        password = args[0];
+                        pwlength = password.length();
+                        entropy = entropy(pwlength);
+                }
+                else{
+		        Scanner scan = new Scanner( System.in );
+		        System.out.print( "Enter a password (numbers and characters, not case sensitive): ");
 
-		String test = scan.next();
-
-		pwlength = test.length();
-
-		entropy = pwlength*(log2(POSSIBLECHARS));
-
-		System.out.println();
-		System.out.println( "The password entered has " + (int)entropy + " bits of entropy.");
+		        password = scan.next();
+		        pwlength = password.length();
+		        entropy = entropy(pwlength);
+		}
+		System.out.println( "The password entered has " + (int)entropy + " bits of entropy." );
 	}
 
 	//does base-2 logarithm
 	public static double log2(double d) {
 		return Math.log(d)/Math.log(2.0);
+	}
+	private static double entropy(int length){
+	        return length*(log2(POSSIBLECHARS));
 	}
 }
